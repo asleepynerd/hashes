303277b682638d07df31806f22b917bd0c2c5d6b
Josh <josh@fluxvy.us>
2023-04-25 10:59:22 +0000

    Add USH - Unicode Secure Hash

--- Makefile
+++ Makefile
@@ -8,7 +8,10 @@ SRC = src
 
 BIN = bin
 
-all: init sha1024 md7
+all: init sha1024 md7 ush
+
+lanman2: $(SRC)/ush.cpp
+	$(CC) $(CFLAGS) $(INCLUDE) -o $(BIN)/ush $(SRC)/ush.cpp
 
 md7: $(SRC)/md7.cpp
 	$(CC) $(CFLAGS) $(INCLUDE) -o $(BIN)/md7 $(SRC)/md7.cpp
---
+++ src/ush.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdint>
+#include <random>
+
+#include "ush.h"
+
+using namespace std;
+
+// Function to generate a secure hash
+string USH(string input)
+{
+	// Initialize the random number generator
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dis(0, 255);
+	
+	// Generate a random 256 bit key
+	vector<uint8_t> key;
+	for (int i = 0; i < 32; i++)
+		key.push_back(dis(gen));
+	
+	// Initialize the hash
+	string hash = "";
+	
+	// Iterate over the input string
+	for (int i = 0; i < input.length(); i++)
+	{
+		// Convert the current character to its unicode value
+		int unicode_val = (int)input[i];
+		
+		// XOR the unicode value with the corresponding byte in the key
+		int xor_val = unicode_val ^ key[i % 32];
+		
+		// Convert the xor value to a hexadecimal string
+		string hex_val = to_string(xor_val);
+		string hex_str = "";
+		for (int j = 0; j < hex_val.length(); j++)
+			hex_str += hex_val[j];
+		
+		// Append the hexadecimal string to the hash
+		hash += hex_str;
+	}
+	
+	return hash;
+}
+
+int main(int argc, char* argv[])
+{
+	// Check for the -v flag
+	if (argc == 2 && string(argv[1]) == "-v")
+	{
+		cout << "Unicode-Secure-Hash (USH)" << VERSION << endl;
+		return 0;
+	}
+	
+	// Check for the correct number of arguments
+	if (argc != 2)
+	{
+		cout << "Usage: ush <input-file-name> [-v]" << endl;
+		return -1;
+	}
+	
+	// Read the input file
+	string input = "";
+	ifstream fin(argv[1]);
+	string line;
+	while (getline(fin, line))
+		input += line;
+	
+	// Generate and print the hash
+	string hash = USH(input);
+	cout << hash << endl;
+	
+	return 0;
+}
